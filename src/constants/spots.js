70526760eb69a2b5189f88509111a551443b6d21
Chad <chapamd18@gmail.com>
2025-11-17 15:50:01 +0800

    Proper Coordinates

--- src/constants/spots.js
+++ src/constants/spots.js
@@ -1,10 +1,10 @@
 export const spotCoordinates = {
-  '1': { lat: 9.8297, lng: 124.1397 }, // Chocolate Hills
-  '2': { lat: 9.6268, lng: 123.8056 }, // Panglao Beach
-  '3': { lat: 9.6912, lng: 123.9529 }, // Tarsier Conservation Area
-  '4': { lat: 9.6348, lng: 124.0298 }, // Loboc River Cruise
-  '5': { lat: 9.6229, lng: 123.9125 }, // Baclayon Church
-  '6': { lat: 10.0623, lng: 124.3421 }, // Trinidad Kawasan Falls
+  '1': { lat: 9.7986, lng: 124.1677, }, // Chocolate Hills
+  '2': { lat: 9.5639, lng: 123.8109 }, // Panglao Beach
+  '3': { lat: 9.6762, lng: 124.0908 }, // Tarsier Conservation Area
+  '4': { lat: 9.6350, lng: 124.0296 }, // Loboc River Cruise
+  '5': { lat: 9.6227, lng: 123.9131 }, // Baclayon Church
+  '6': { lat: 10.0084, lng: 124.3421 }, // Trinidad Kawasan Falls
   '7': { lat: 9.6273, lng: 123.8788 }, // Blood Compact Shrine
   '8': { lat: 9.5757, lng: 123.8271 }, // Bohol Bee Farm
   '9': { lat: 9.7417, lng: 124.5758 }, // Anda White Long Beach
